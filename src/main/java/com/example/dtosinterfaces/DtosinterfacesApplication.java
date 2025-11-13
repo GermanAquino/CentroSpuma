7dbf6391da6b2809fac6e80bef79771d295fa8b0
GermanAquino <german.aquino2019@fiuni.edu.py>
2025-11-12 22:34:24 -0300

    Proyecto base

---
+++ src/main/java/com/example/dtosinterfaces/DtosinterfacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dtosinterfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtosinterfacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtosinterfacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dtosinterfaces/DtosinterfacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dtosinterfaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtosinterfacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
